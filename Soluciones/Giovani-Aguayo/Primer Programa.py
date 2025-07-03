2a58c5c35cfd9ac8e17c8de39cc3b4d3eadf04cc
GiovaniAguayo <123456giovani@gmail.com>
2025-07-02 22:22:59 -0600

    3ra Clase

---
+++ Soluciones/Giovani-Aguayo/Primer Programa.py
@@ -0,0 +1,12 @@
+
+def numerosEnteros(parametroUno, parametroDos):
+	for i in range(100):
+		if i%3==0:
+			print (parametroUno)
+			if i%5==0:
+				print(parametroDos)
+			else 
+			print(parametroDos)
+	
+
+numerosEnteros("Multiplo de 3", "Multiplo de 5")
