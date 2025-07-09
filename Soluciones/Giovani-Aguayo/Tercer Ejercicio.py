6b086c8d1bb4e8e6d889d6bb588e575d2ca0a5db
GiovaniAguayo <123456giovani@gmail.com>
2025-07-08 23:12:01 -0600

    clase 5

--- Soluciones/Giovani-Aguayo/Tercer Ejercicio.py
+++ Soluciones/Giovani-Aguayo/Tercer Ejercicio.py
@@ -1,51 +1,59 @@
 def convertirDecimalARomano():
-	numero=int(input("Introduce un numero decimal"))
+    numero = int(input("Introduce un numero decimal"))
 
-	valores=[(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
-			(10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
-	]
+    valores = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I"),
+    ]
 
-	resultado= ""
+    resultado = ""
 
-	for valor, simbolo in valores:
-		while numero >= valor:
-			resultado += simbolo
-			numero -= valor
-	return resultado
+    for valor, simbolo in valores:
+        while numero >= valor:
+            resultado += simbolo
+            numero -= valor
+    return resultado
 
 
 def convertirRomanoADecimal():
-	numero=input("Introduce un numero romano")
+    numero = input("Introduce un numero romano")
 
-	valores={   'I': 1, 'V': 5, 'X': 10,
-        'L': 50, 'C': 100, 'D': 500, 'M': 1000
-	}
+    valores = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-	total = 0
-	i= 0
+    total = 0
+    i = 0
 
-	while i < len(numero):
-		valor1=valores[numero[i]]
+    while i < len(numero):
+        valor1 = valores[numero[i]]
 
-		if i + 1 < len(numero):
-			valor2=valores[numero[i+1]]
+        if i + 1 < len(numero):
+            valor2 = valores[numero[i + 1]]
 
-			if valor1 < valor2: 
-				total+=valor2-valor1
-				i+=2 
+            if valor1 < valor2:
+                total += valor2 - valor1
+                i += 2
 
-			else:
-				total+= valor1 
-				i+=1   
-		else:
-			total+=valor1   
-			i+= 1
+            else:
+                total += valor1
+                i += 1
+        else:
+            total += valor1
+            i += 1
 
-	return total
+    return total
 
 
+print(convertirRomanoADecimal())
 
-
-print( convertirRomanoADecimal())
-
-print( convertirDecimalARomano())
+print(convertirDecimalARomano())
