b8df969e361b1e67e379bbfc422d1cd4012754ef
Diego <diegoo9997@gmail.com>
2025-07-27 10:12:41 -0600

    clase 27-07-2025

--- Notas-Clases/26-07-2025/Herencia.py
+++ Notas-Clases/26-07-2025/Herencia.py
@@ -1,33 +1,95 @@
 class Hierba:
     atributo = "hojas"
-    def __init__(self, ataque_hierba):
+    __debilidad = "Fuego"
+    _var ="ihadfesgbiua"
+
+    def __init__(self, ataque_hierba, ataque_otro_tipo = ""):    
+        self.__var = "hola"   
         self.ataque_hierba = ataque_hierba
-        pass
+        try:
+            super().__init__(ataque_otro_tipo)
+        except:
+            pass
+
+    @classmethod  
+    def get_debilidad(cls):
+        return cls.__debilidad 
+    
+    def get____var(self):
+        return self.__var
+
+    def atacar(self):
+        try:
+            self.ataque = super().atacar() 
+            return f"{self.ataque} ataca con hojas"
+        except:
+            return "ataca con hojas"
         
 
 
 class Fuego: 
     atributo = "llamas"
-    def __init__(self, ataque_fuego, ataque_hierba):
+    def __init__(self, ataque_fuego, ataque_otro_tipo = ""):
+        
         self.ataque_fuego = ataque_fuego
-        super().__init__(ataque_hierba)
-        pass
+        try:
+            super().__init__(ataque_otro_tipo)
+        except:
+            pass
+    
+    def atacar(self):
+        try:
+            self.ataque = super().atacar() 
+            return f"{self.ataque} y ataca con llamas"
+        except:
+            return "ataca con llamas"
         
 
-class Pokemon(Fuego, Hierba):   
+class HierbaFuego(Hierba, Fuego):   
+    def __init__(self, name, ataque_hierba, ataque_fuego):
+        super().__init__(ataque_hierba, ataque_fuego)
+        print(self.ataque_hierba, self.ataque_fuego)
+        self.name = name
+    
+    def atacar(self):
+        self.ataque = super().atacar()
+        print(f"HierbaFuego:  {self.name} {self.ataque}")
+
+class FuegoHierba(Fuego, Hierba):
     def __init__(self, name, ataque_fuego, ataque_hierba):
         super().__init__(ataque_fuego, ataque_hierba)
         print(self.ataque_fuego, self.ataque_hierba)
         self.name = name
+    
+    def atacar(self):
+        self.ataque = super().atacar()
+        print(f"FuegoHierba:  {self.name} {self.ataque}")
 
-print(Pokemon.__mro__)
+        
 
-print(Pokemon.atributo)
+print(HierbaFuego.__mro__)
+print(FuegoHierba.__mro__)
 
-print(Pokemon.atributo)
+print(HierbaFuego.atributo)
 
-pokemon = Pokemon("charizard", "lanzallamas", "hojas navaja")
+print(HierbaFuego.atributo)
 
-print(pokemon.name)
+hierbaFuego = HierbaFuego("charizard",  "hojas navaja", "lanzallamas")
+
+fuegohierba = FuegoHierba("venasaur", "lanzallamas", "hojas navaja")
+
+print(hierbaFuego.name)
+
+hierbaFuego.atacar()
+
+fuegohierba.atacar()
+
+print(Hierba.get_debilidad())
+
+print(Hierba._var)
+
+print(fuegohierba.get____var())
+
+print(fuegohierba.__var)
 
 
